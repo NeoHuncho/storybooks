756ec57c934ef06e5142a7a0bedfb2d91f330f58
NeoHuncho <william.guinaudie@gmail.com>
2020-11-06 22:49:22 +0100

    implemented google sign in

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
+const path = require('path')//core Node.Js module
 const express = require('express')
 const dotenv = require('dotenv')
-
-
+const morgan = require('morgan')
+const connectDB = require('./config/db')
+const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+//load config
 dotenv.config({ path: './config/config.env'})
 
-const app = express();
+//passport config
+require('./config/passport',passport) 
 
-const PORT = process.env.PORT || 5000
+connectDB()
+
+const app = express();
+//logging
+if(process.env.NODE_ENV=== 'development'){
+     app.use(morgan('dev'))
+}
+//Handlebars
+app.engine('.hbs',exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine','.hbs');
+
+//Sessions
+
+app.use(session({
+     secret: 'keyboard cat',
+     resave: false,
+     saveUninitialized: false
+   }))
+//Passoport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// static folder
+app.use(express.static(path.join(__dirname,'public')))
+
+//Routes
+app.use('/',require('./routes/index'))
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT,
-     console.log(`Server running in ${process.env.NODE_ENV} mode one port ${PORT}`)
+     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
      )
